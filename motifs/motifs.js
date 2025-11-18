43fc4d5681c2329bf58082a993e212c86301d637
MonstyrSlayr <eehikioya0@gmail.com>
2025-11-18 17:28:42 -0500

    updated motif sorting

--- motifs/motifs.js
+++ motifs/motifs.js
@@ -1,6 +1,27 @@
-import { allMotifs, createMotifDiv } from "../data.js";
+import { allMotifs, allSongs, createMotifDiv, getSongsWithMotif } from "../data.js";
 
 const allMotifsDiv = document.getElementById("motifList");
+
+// sort motif by order they appear in the song list
+// if they don't appear, kill self
+allMotifs.sort((a, b) =>
+{
+    if (!getSongsWithMotif(a) && !getSongsWithMotif(b)) return 0;
+    if (!getSongsWithMotif(a)) return 1;
+    if (!getSongsWithMotif(b)) return -1;
+
+    // stupid
+    for (const song of allSongs)
+    {
+        for (const motifRef of song.motifRefs)
+        {
+            if (motifRef.motif == a) return -1;
+            if (motifRef.motif == b) return 1;
+        }
+    }
+
+    return 0;
+})
 
 const allMotifIds = new Set();
 for (const motif of allMotifs)
