0add35dbce2bd28f8d29f9d213ad6b47a981de57
Nathan Ehikioya <59293011+MonstyrSlayr@users.noreply.github.com>
2026-08-13 04:45:05 -0400

    what

--- contribData.js
+++ contribData.js
@@ -38,5 +38,5 @@ Contributors.KARMA.discord = "https://discord.com/users/315969740419891200";
 Contributors.KARMA.youtube = "https://www.youtube.com/@thatkarmaguy5356";
 
 Contributors.TERVIA = new Contributor("Tervia", null, "");
-Contributors.TERVIA.github = "https://github.com/Tervia"
-Contributors.TERVIA.discord = "https://discord.com/users/169656943122579456"
+Contributors.TERVIA.github = "https://github.com/Tervia";
+Contributors.TERVIA.discord = "https://discord.com/users/169656943122579456";
